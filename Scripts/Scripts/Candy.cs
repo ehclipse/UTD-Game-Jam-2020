603cccb5283e89d69b4e7e6e9762e29495f57b67
cao-eric <73639302+cao-eric@users.noreply.github.com>
2020-10-29 22:32:11 -0500

    Add files via upload

---
+++ Scripts/Scripts/Candy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candy : MonoBehaviour
+{
+    public int candyValue = 100; //temporary value for Candy. Can modify later
+
+    private void OnTriggerEnter2D(Collider2D other) //if Candy object collides with 
+    {
+	     if(other.gameObject.CompareTag("Player")) //an object tagged Player
+	    {
+	        CandyScore.obj.UpdateScore(candyValue); //Update the score
+            Destroy(this.gameObject); //destroy the candy object
+        }
+    }
+
+
+}
---
+++ Scripts/Scripts/CandyScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CandyScore : MonoBehaviour
+{
+    public static CandyScore obj;
+    public TextMeshProUGUI text;
+    int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(obj == null) //if obj doesnt exist
+	    {
+	        obj = this; //instantiate it
+	    }
+    }
+
+    public void UpdateScore(int candyValue) 
+    {
+    	score += candyValue; //takes in the candy value and adds it to score
+    	text.text = "Score: " + score.ToString(); //updates the display for score
+    }
+}
